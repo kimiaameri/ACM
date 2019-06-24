b56250cd76873658b12d0046f6d6fa15a38d6581
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-24 16:58:46 -0500

    Update pythonBwa.py

--- pythonBwa.py
+++ pythonBwa.py
@@ -15,5 +15,5 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'{minicondaBin}bwa mem $WORK/SANVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SANVA-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SANVA-outputs/trimmomatic/{row[0]}-R2.paired.fq >$WORK/SANVA-outputs/samfiles/{row[0]}.sam\n')
+                outFile.write(f'{minicondaBin}bwa mem $WORK/ACM_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/ACM-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/ACM-outputs/trimmomatic/{row[0]}-R2.paired.fq >$WORK/ACM-outputs/samfiles/{row[0]}.sam\n')
             count =count+1
